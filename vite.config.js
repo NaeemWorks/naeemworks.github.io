2cd701de5bb8b71f7907ebe3a3866c02a141a17a
NaeemWorks <naeemk.works@gmail.com>
2025-07-23 12:23:04 -0400

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace below with your GitHub repo name
-const repoName = "naeemworks.github.io";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
